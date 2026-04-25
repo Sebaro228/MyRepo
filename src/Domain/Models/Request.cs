96febf785b3a877548179ad72cd4f1589db43a08
Sebaro228 <sebalebid3@gmail.com>
2026-04-25 10:08:05 +0300

    models

---
+++ src/Domain/Models/Request.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+namespace Domain.Models
+{
+    public enum AiProvider { OpenAI, Claude, TTS }
+    public class Request
+    {
+        public string Id { get; set; }
+        public AiProvider Provider { get; set; }
+        public string PromptText { get; set; }
+        public string? ResponceText { get; set; }
+        public int TokenUsed { get; set; }
+        public bool IsSuccessful { get; set; }
+        public string? ErrorMessage { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
